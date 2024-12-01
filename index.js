fd6e12e0f400aec71fe16150cdc7c5307bf72893
yaasir cheekoory <yaasir1997@gmail.com>
2024-12-02 00:39:22 +0400

    using cdn

---
+++ index.js
@@ -0,0 +1,28 @@
+import { createApp } from "vue";
+
+createApp({
+  data() {
+    return {
+      message:
+        "If you see this message in your browser, that means Vue is successfully mounted! 🙌",
+      todos: [
+        { title: "Code a to-do list", done: false },
+        { title: "Eat breakfast", done: true },
+        { title: "Do some exercise", done: false },
+        { title: "Water the plants", done: true },
+      ],
+      newTodo: null,
+    };
+  },
+  methods: {
+    handleAdd() {
+      if (this.newTodo) {
+        this.todos.push({ title: this.newTodo, done: false });
+        this.newTodo = null;
+      }
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1)
+    },
+  },
+}).mount("#todosContainer");
